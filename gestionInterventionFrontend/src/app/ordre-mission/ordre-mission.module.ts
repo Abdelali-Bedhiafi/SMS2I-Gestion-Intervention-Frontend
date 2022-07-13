b7e4d5eaba3ed1489d6eb4b5900dca4d79faf567
DESKTOP-0PN26UR\abdelali <abdelalibedhiafi123@gmail.com>
2022-07-13 12:00:44 +0100

    ordre-mission module

---
+++ gestionInterventionFrontend/src/app/ordre-mission/ordre-mission.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OrdreMissionRoutingModule } from './ordre-mission-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    OrdreMissionRoutingModule
+  ]
+})
+export class OrdreMissionModule { }
