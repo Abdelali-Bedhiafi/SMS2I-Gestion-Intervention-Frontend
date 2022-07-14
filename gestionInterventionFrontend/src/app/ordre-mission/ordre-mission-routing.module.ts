44477ebc26a044ed9d0050171774b09d276dee43
ahmed-ghariani <ahmed.ghariani@etudiant-fst.utm.tn>
2022-07-14 17:42:12 +0100

    correction

--- gestionInterventionFrontend/src/app/ordre-mission/ordre-mission-routing.module.ts
+++ gestionInterventionFrontend/src/app/ordre-mission/ordre-mission-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreationOrdreMissionComponent } from './creation-ordre-mission/creation-ordre-mission.component';
 import { DetailOrdreMissionComponent } from './detail-ordre-mission/detail-ordre-mission.component';
 
 const routes: Routes = [
-
-  {
-    path: "details",component:DetailOrdreMissionComponent
-  }
-
+  { path: "details", component: DetailOrdreMissionComponent },
+  { path: "creation", component: CreationOrdreMissionComponent }
 ];
 
 @NgModule({
