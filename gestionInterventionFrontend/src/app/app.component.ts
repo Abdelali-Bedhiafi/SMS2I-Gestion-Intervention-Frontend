d8fbbf5e869c1f0625b4674d9a813eb369be72cf
DESKTOP-0PN26UR\abdelali <abdelalibedhiafi123@gmail.com>
2022-07-14 12:00:35 +0100

    clientService

--- gestionInterventionFrontend/src/app/app.component.ts
+++ gestionInterventionFrontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ClientService } from './client.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gestionInterventionFrontend';
+  constructor(public client: ClientService){}
 }
