e2a2a5e87238ae0afce078e1e983d22efc99030f
DESKTOP-0PN26UR\abdelali <abdelalibedhiafi123@gmail.com>
2022-07-14 08:09:07 +0100

    html,css creation ordre mission

--- gestionInterventionFrontend/src/app/ordre-mission/ordre-mission.module.ts
+++ gestionInterventionFrontend/src/app/ordre-mission/ordre-mission.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OrdreMissionRoutingModule } from './ordre-mission-routing.module';
+import { CreationOrdreMissionComponent } from './creation-ordre-mission/creation-ordre-mission.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CreationOrdreMissionComponent
+  ],
   imports: [
     CommonModule,
     OrdreMissionRoutingModule
