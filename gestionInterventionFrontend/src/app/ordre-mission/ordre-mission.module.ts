8f80799354be2d2473b677798c92d789ac8158f2
ahmed-ghariani <ahmed.ghariani@etudiant-fst.utm.tn>
2022-07-14 17:35:05 +0100

    correction

--- gestionInterventionFrontend/src/app/ordre-mission/ordre-mission.module.ts
+++ gestionInterventionFrontend/src/app/ordre-mission/ordre-mission.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { OrdreMissionRoutingModule} from './ordre-mission-routing.module';
+import { OrdreMissionRoutingModule } from './ordre-mission-routing.module';
 import { CreationOrdreMissionComponent } from './creation-ordre-mission/creation-ordre-mission.component';
 import { DetailOrdreMissionComponent } from './detail-ordre-mission/detail-ordre-mission.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatCardModule} from '@angular/material/card';
-import {MatSelectModule} from '@angular/material/select';
-import {MatListModule} from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule} from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatListModule } from '@angular/material/list';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -23,6 +25,9 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
     MatCardModule,
     MatToolbarModule,
     MatGridListModule,
