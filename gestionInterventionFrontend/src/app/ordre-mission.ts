43a74229b7e00bef03f2de852b579f994976a6ad
ahmed-ghariani <ahmed.ghariani@etudiant-fst.utm.tn>
2022-07-16 12:25:43 +0100

    categories

--- gestionInterventionFrontend/src/app/ordre-mission.ts
+++ gestionInterventionFrontend/src/app/ordre-mission.ts
@@ -1,11 +1,12 @@
 import { Client } from "./client";
 import { Technicien } from "./technicien";
+import {SousCategorie} from "./sous-categorie";
 
 export interface OrdreMission {
   id: number;
   client: Client;
-  status: string;
-  dateMission: Date;
+  etat: string;
   dateDebutEstimee: Date;
   techniciens: Technicien[];
+  sousCategories: SousCategorie[];
 }
---
+++ gestionInterventionFrontend/src/app/sous-categorie.ts
@@ -0,0 +1,9 @@
+import {Categorie} from "./categorie";
+
+export interface SousCategorie {
+  id: number;
+  titre: string ;
+  description: string ;
+  categorie: Categorie;
+
+}
