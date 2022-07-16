d1d2ca5f9d8d4e340df7dc2c93d512e52cb4380b
ahmed-ghariani <ahmed.ghariani@etudiant-fst.utm.tn>
2022-07-16 18:47:06 +0100

    categories

--- gestionInterventionFrontend/src/app/test/test.component.ts
+++ gestionInterventionFrontend/src/app/test/test.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import {OrdreMissionService} from "../ordre-mission.service";
+import {CheckListService} from "../service/check-list.service";
+import {CheckList} from "../model/check-list";
+import {Software} from "../model/software";
+import {SoftwareService} from "../service/software.service";
+import {SoftwareCategorieService} from "../service/software-categorie.service";
 
 @Component({
   selector: 'app-test',
@@ -9,11 +12,42 @@ import {OrdreMissionService} from "../ordre-mission.service";
 })
 export class TestComponent implements OnInit {
 
-  missionReady = false;
-  constructor(public mission$: OrdreMissionService) { }
+  checklist!: CheckList;
+  softwareMap: Map<string, Software[]> = new Map<string, Software[]>();
+  notAllCategoriesChecked = false;
+  ready= false;
+
+  constructor(private checkList$: CheckListService,
+              private categorie$: SoftwareCategorieService) { }
 
   ngOnInit(): void {
-    this.mission$.ready.then( ready=> this.missionReady=ready);
+      this.checkList$.getById(10).subscribe(checklist=>{
+        this.checklist=checklist;
+        this.checkCategorie(checklist.model.softwareCategories);
+        this.ready=true;
+      });
+  }
+  checkCategorie(categories:[{ id: number; nom: string;}]){
+    for (const categorie of categories) {
+      let checked = false;
+      for (const software of this.checklist.softwares) {
+        if(software.categorie==categorie.nom){
+          checked=true;
+          break;
+        }
+        if(!checked){
+          this.notAllCategoriesChecked = true;
+          this.categorie$.getById(categorie.id).subscribe(c => this.softwareMap.set(c.nom,c.softwares) );
+          }
+        }
+      }
   }
 
+
+
+}
+
+interface Categorie{
+  id: number;
+  nom: string;
 }
