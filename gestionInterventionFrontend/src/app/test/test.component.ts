3fbb1997c30f32b5eb5274d504696afb04732b6d
ahmed-ghariani <ahmed.ghariani@etudiant-fst.utm.tn>
2022-07-17 14:56:55 +0100

    init

--- gestionInterventionFrontend/src/app/test/test.component.ts
+++ gestionInterventionFrontend/src/app/test/test.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {CheckListService} from "../service/check-list.service";
-import {CheckList} from "../model/check-list";
-import {Software} from "../model/software";
-import {SoftwareCategorieService} from "../service/software-categorie.service";
-import {FormControl} from "@angular/forms";
+
 
 @Component({
   selector: 'app-test',
@@ -12,77 +8,9 @@ import {FormControl} from "@angular/forms";
 })
 export class TestComponent implements OnInit {
 
-  checklist!: CheckList;
-  softwareMap: Map<string,{softwares:Software[],input:FormControl<Software>}> =new Map<string, {softwares: Software[]; input: FormControl<Software>}>();
-  notAllCategoriesChecked = false;
-  ready= false;
-  constructor(private checkList$: CheckListService,
-              private categorie$: SoftwareCategorieService) { }
+  constructor() { }
 
   ngOnInit(): void {
-      this.checkList$.getById(14).subscribe(checklist=>{
-        this.checklist=checklist;
-        this.checkCategorie(checklist.model);
-        this.ready=true;
-      });
-  }
-  checkCategorie(model:{id: number, softwareCategories: {id: number, nom: string}[], softwares: {id: number}[]}) {
-    // seulement les software specifie dans le model existe dans le chacklist
-    if (model.softwares.length == this.checklist.softwares.length) {
-      this.notAllCategoriesChecked = true;
-      for (const categorie of model.softwareCategories) {
-        this.categorie$.getById(categorie.id).subscribe(c => this.softwareMap.set(c.nom, {
-          softwares: c.softwares,
-          input: new FormControl()
-        }));
-      }
-    }
-    // pour que la liste des software depace celle de model tous les categorie doit etre specfiee dabord
-    else if((model.softwareCategories.length + model.softwares.length)> this.checklist.softwares.length){
-      this.notAllCategoriesChecked = true;
-      // tous les software no specifies dans le model (liste des softwares de model)
-      let addedSoftware: Software[] = [];
-      // remplir la liste addedSoftware
-      for (const software of this.checklist.softwares) {
-        let added = true;
-        for (const modelSoftware of model.softwares) {
-          if (software.id== modelSoftware.id) added=false;
-        }
-        if(added) addedSoftware.push(software);
-      }
-      // parcourir l ensemble des categories du model afin de prendre les categories non encore specifie
-      for (const categorie of model.softwareCategories) {
-        // parcourir la liste des addedSoftware si aucun de ces software a la meme categorie alors creation du form de selection software
-        let checked = false;
-        for (const software of addedSoftware) {
-          if (software.categorie == categorie.nom) {
-            checked = true;
-            break;
-          }
-        }
-        if(!checked){
-          this.categorie$.getById(categorie.id).subscribe(c => this.softwareMap.set(c.nom,{softwares:c.softwares,input: new FormControl()}) );
-        }
-      }
-    }
-
-  }
-
-  selectSoftware(categorie: string){
-    let selectedSoftware =this.softwareMap.get(categorie)?.input.value;
-    if (selectedSoftware != undefined){
-      const newChecklist:CheckList = { id:this.checklist.id,materiels:this.checklist.materiels,softwares:[],model:this.checklist.model};
-      this.checklist.softwares.forEach(software =>newChecklist.softwares.push(software));
-      newChecklist.softwares.push(selectedSoftware);
-      this.checkList$.update(this.checklist.id,newChecklist).subscribe(checkList => {
-        if (this.checklist.softwares.length < checkList.softwares.length) {
-          console.log("here");
-          this.softwareMap.delete(categorie);
-          this.checklist=checkList;
-        }
-      });
-    }
-
   }
 
 
