de464a73a2744081bbcee4a32672fab7faf0380c
Anas Wahada <anas.wahada@esprit.tn>
2022-07-18 11:19:19 +0100

    detaiBonIntervention

---
+++ gestionInterventionFrontend/src/app/ordre-mission/detail-bon-intervention/detail-bon-intervention.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-bon-intervention',
+  templateUrl: './detail-bon-intervention.component.html',
+  styleUrls: ['./detail-bon-intervention.component.css']
+})
+export class DetailBonInterventionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- gestionInterventionFrontend/src/app/ordre-mission/ordre-mission-routing.module.ts
+++ gestionInterventionFrontend/src/app/ordre-mission/ordre-mission-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreationOrdreMissionComponent } from './creation-ordre-mission/creation-ordre-mission.component';
+import { DetailBonInterventionComponent } from './detail-bon-intervention/detail-bon-intervention.component';
 import { DetailOrdreMissionComponent } from './detail-ordre-mission/detail-ordre-mission.component';
 
 const routes: Routes = [
   { path: "details", component: DetailOrdreMissionComponent },
-  { path: "creation", component: CreationOrdreMissionComponent }
+  { path: "creation", component: CreationOrdreMissionComponent },
+  {path:"detailBon",component:DetailBonInterventionComponent}
 ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
